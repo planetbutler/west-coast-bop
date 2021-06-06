861447c3e63c3031ded3a72e7183f1dfaf2f3e5a
Paul Butler <planetbutler@gmail.com>
2021-06-06 11:20:34 -0400

    wired up time counter

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPause, faAngleLeft, faAngleRight, faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
@@ -16,21 +16,40 @@ const playTuneHandler = () => {
         setIsPlaying(!isPlaying);
     }
 }
+const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setTuneInfo({...tuneInfo, currentTime: current, duration});
+}
+const getTime = (time) => {
+    return(
+        Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+}
+//state
+const [tuneInfo, setTuneInfo] = useState({
+    currentTime: null,
+    duration: null,
+})
 
     return (
         <div className="music-player">
             <div className="horiz-control">
                 <div className="time-control">
-                    <p>Start Time</p>
+                    <p>{getTime(tuneInfo.currentTime)}</p>
                     <input type="range" />
-                    <p>End Time</p>
+                    <p>{getTime(tuneInfo.duration)}</p>
                 </div>
                 <div className="play-control">
                     <FontAwesomeIcon className="skip-back" size="2x" icon={ faAngleLeft } />
                     <FontAwesomeIcon onClick={ playTuneHandler} className="play" size="2x" icon={ faPlay } />
                     <FontAwesomeIcon className="skip-forward" size="2x" icon={ faAngleRight } />
                 </div>
-                <audio ref={audioRef} src={currentArtist.tune}></audio>
+                <audio 
+                onLoadedMetadata={timeUpdateHandler}
+                onTimeUpdate={timeUpdateHandler} 
+                ref={audioRef} 
+                src={currentArtist.tune}></audio>
             </div>
             <div className="volume-control">
                 <div className="vc-input">
