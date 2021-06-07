8f99a1d018d1c562cc4c760db6c1b17f5d7e0771
Paul Butler <planetbutler@gmail.com>
2021-06-07 06:18:42 -0400

    wired up volume and mute

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Artist = ({currentArtist}) => {
     return (
         <div className="artist">
-            <img src={currentArtist.vid} alt="video" />
+            <img src={currentArtist.vid} alt={currentArtist.name} />
             <h2>{currentArtist.name}</h2>
             <h3>{currentArtist.title}</h3>
         </div>
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -26,37 +26,79 @@ const getTime = (time) => {
         Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
 }
+const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setTuneInfo({...tuneInfo, currentTime: e.target.value})
+};
+
 //state
 const [tuneInfo, setTuneInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: 0,
+    duration: 0,
 })
+
+const [isMuted, setIsMuted] = useState(false)
+
+const audioMute = () => {
+    if(audioRef.current.muted) {
+        audioRef.current.muted = false;
+        setIsMuted(!isMuted)
+    } else {
+        audioRef.current.muted = true;
+        setIsMuted(!isMuted)
+    }
+}
+
+const setVolume = (e) => {
+    audioRef.current.volume = e.target.value / 100;
+}
+
 
     return (
         <div className="music-player">
             <div className="horiz-control">
                 <div className="time-control">
                     <p>{getTime(tuneInfo.currentTime)}</p>
-                    <input type="range" />
+                    <input 
+                    min={0} 
+                    max={tuneInfo.duration} 
+                    onChange={dragHandler}
+                    value={tuneInfo.currentTime} 
+                    type="range" />
                     <p>{getTime(tuneInfo.duration)}</p>
                 </div>
                 <div className="play-control">
                     <FontAwesomeIcon className="skip-back" size="2x" icon={ faAngleLeft } />
-                    <FontAwesomeIcon onClick={ playTuneHandler} className="play" size="2x" icon={ faPlay } />
+                    <FontAwesomeIcon onClick={ playTuneHandler} 
+                    className="play" 
+                    size="2x" 
+                    icon={isPlaying ? faPause : faPlay } />
                     <FontAwesomeIcon className="skip-forward" size="2x" icon={ faAngleRight } />
                 </div>
                 <audio 
-                onLoadedMetadata={timeUpdateHandler}
-                onTimeUpdate={timeUpdateHandler} 
-                ref={audioRef} 
-                src={currentArtist.tune}></audio>
+                    onLoadedMetadata={timeUpdateHandler}
+                    onTimeUpdate={timeUpdateHandler} 
+                    ref={audioRef} 
+                    src={currentArtist.tune}>
+                </audio>
             </div>
             <div className="volume-control">
                 <div className="vc-input">
-                    <input type="range" />
+                    <input 
+                    min={0} 
+                    max={100} 
+                    defaultValue={50} 
+                    step={1}
+                    onChange={setVolume}
+                    type="range" />
                 </div>
-                <div className="mute">
-                    <FontAwesomeIcon className="mute" size="2x" icon={ faVolumeUp } />
+                <div>
+                    <button onClick={() => audioMute()}>
+                        <FontAwesomeIcon 
+                        className="mute" 
+                        size="2x" 
+                        icon={ isMuted ? faVolumeMute : faVolumeUp } />
+                    </button>
                 </div>
             </div>
         </div>
